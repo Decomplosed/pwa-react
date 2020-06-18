6e4026119c39e9937fc3198be3ea6e2e9cc6ba43
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-18 21:50:00 +0200

    added API_KEY

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const url = 'https://api.openweathermap.org/data/2.5/weather'
+const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = 'f33a484cf794d08d0148764789aaba32'
 
 const fetchWeather = async (query) => {
   const response = await axios.get()
