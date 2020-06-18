6a887b8145451cfc4fed7edc02089c7f425da290
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-18 21:49:26 +0200

    defined url

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,3 +1,7 @@
 import axios from 'axios'
 
-const fetchWeather = async (query) => {}
+const url = 'https://api.openweathermap.org/data/2.5/weather'
+
+const fetchWeather = async (query) => {
+  const response = await axios.get()
+}
